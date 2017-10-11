c3e3d71c420cf99ae427658bc2352ffac732c5e4
林竜之介 <m011434688@edu.teu.ac.jp>
2017-10-11 15:54:42 +0900

    設定追加

---
+++ Assets/Editor/CreateAssetBundle.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public class CreateAssetBundle{
+
+    [MenuItem("Assets/Buid AssetBundle")]
+    static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundle", BuildAssetBundleOptions.None, BuildTarget.WebGL);
+    }
+}
---
+++ Assets/Hayashi/UtilityAssets/Scripts/GameSetting.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetting : MonoBehaviour {
+
+    const string OBJECTPATH = "Prefabs/";
+    const string SOUNDPATH = "Sound/";
+    const string BGMPATH = SOUNDPATH + "BGM/";
+    const string SEPATH = SOUNDPATH + "SE/";
+}
