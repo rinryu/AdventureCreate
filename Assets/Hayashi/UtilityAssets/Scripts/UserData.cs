80e54319b4cc864c67b7f047ff8c9be47792ad71
林竜之介 <m011434688@edu.teu.ac.jp>
2017-11-05 12:08:49 +0900

    [クラス追加]ユーザーデータ格納用のクラス追加

---
+++ Assets/Hayashi/UtilityAssets/Scripts/UserData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserData {
+	private static UserData _Instanse;
+
+	private UserData(){
+
+	}
+	public static UserData Instanse{
+		get{
+			if(_Instanse == null){
+				_Instanse = new UserData();
+			}
+			return _Instanse;
+
+		}
+	}
+
+	public string username;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
